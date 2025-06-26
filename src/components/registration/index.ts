aeecbbdde388fb266e0a7ad444914bfcdc16d9b0
Ramsey Feng <ramsey.feng@roche.com>
2025-06-26 14:39:13 +0800

    refactor(app): remove unused components and clean up styles

--- src/components/registration/index.ts
+++ src/components/registration/index.ts
@@ -1,5 +1,2 @@
 // Barrel file for registration form components
 export { default as RegistrationForm } from './RegistrationForm'
-export { default as Step1BasicInfo } from './Step1BasicInfo'
-export { default as Step2PersonalDetails } from './Step2PersonalDetails'
-export { default as Step3AccountInfo } from './Step3AccountInfo'
