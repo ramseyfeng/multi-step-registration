28e8931ba323cfbebd9e772ee8bcf0a2a80a9603
Ramsey Feng <ramsey.feng@roche.com>
2025-07-03 13:26:21 +0800

    build: update husky installation command and optimize Rollup manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,21 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          mui: [
+            '@mui/material',
+            '@mui/icons-material',
+            '@mui/x-date-pickers',
+            '@emotion/react',
+            '@emotion/styled',
+          ],
+          datefns: ['date-fns'],
+        },
+      },
+    },
+  },
 })
